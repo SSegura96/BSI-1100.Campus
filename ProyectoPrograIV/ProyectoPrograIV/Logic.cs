d41a2ac977e4f0e2bae8f91b884dfcd9f7b914e7
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2016-03-12 23:58:08 -0600

    *

---
+++ ProyectoPrograIV/ProyectoPrograIV/Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProyectoPrograIV
+{
+    class Logic
+    {
+        public Logic (String _query)
+        {
+            conectaAccess(_query);
+        }
+
+        private void conectaAccess(String query)
+        {
+            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+ Directory.GetCurrentDirectory()+"\\Escuela.accdb");
+            OleDbCommand orden = new OleDbCommand(query, conexion);
+            orden.Connection.Open();
+            orden.ExecuteNonQuery();
+            orden.Connection.Close();
+        }
+    }
+}
