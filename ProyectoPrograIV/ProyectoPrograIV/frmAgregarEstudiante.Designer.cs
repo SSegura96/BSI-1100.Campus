0f20b106757638dd8b7f6347de768ab1a4bffd72
Javier Fernández <javier_f13@outlook.com>
2016-04-01 16:15:54 -0600

    Se agrega el form frmAgregarEstudiante

---
+++ ProyectoPrograIV/ProyectoPrograIV/frmAgregarEstudiante.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ProyectoPrograIV
+{
+    partial class frmAgregarEstudiante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtGrupo = new System.Windows.Forms.TextBox();
+            this.lblGrupo = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCarne = new System.Windows.Forms.TextBox();
+            this.lblCarne = new System.Windows.Forms.Label();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(193, 174);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 33);
+            this.btnLimpiar.TabIndex = 19;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtGrupo
+            // 
+            this.txtGrupo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrupo.Location = new System.Drawing.Point(84, 138);
+            this.txtGrupo.MaxLength = 3;
+            this.txtGrupo.Name = "txtGrupo";
+            this.txtGrupo.Size = new System.Drawing.Size(289, 23);
+            this.txtGrupo.TabIndex = 18;
+            this.txtGrupo.TextChanged += new System.EventHandler(this.txtGrupo_TextChanged);
+            // 
+            // lblGrupo
+            // 
+            this.lblGrupo.AutoSize = true;
+            this.lblGrupo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrupo.Location = new System.Drawing.Point(26, 141);
+            this.lblGrupo.Name = "lblGrupo";
+            this.lblGrupo.Size = new System.Drawing.Size(52, 17);
+            this.lblGrupo.TabIndex = 17;
+            this.lblGrupo.Text = "Grupo:";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorreo.Location = new System.Drawing.Point(84, 109);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(289, 23);
+            this.txtCorreo.TabIndex = 16;
+            this.txtCorreo.TextChanged += new System.EventHandler(this.txtCorreo_TextChanged);
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.Location = new System.Drawing.Point(23, 112);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(55, 17);
+            this.lblCorreo.TabIndex = 15;
+            this.lblCorreo.Text = "Correo:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(84, 80);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(289, 23);
+            this.txtNombre.TabIndex = 14;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(16, 83);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(62, 17);
+            this.lblNombre.TabIndex = 13;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(112, 174);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 33);
+            this.btnAgregar.TabIndex = 12;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtCarne
+            // 
+            this.txtCarne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCarne.Location = new System.Drawing.Point(84, 51);
+            this.txtCarne.Name = "txtCarne";
+            this.txtCarne.Size = new System.Drawing.Size(289, 23);
+            this.txtCarne.TabIndex = 11;
+            // 
+            // lblCarne
+            // 
+            this.lblCarne.AutoSize = true;
+            this.lblCarne.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarne.Location = new System.Drawing.Point(28, 54);
+            this.lblCarne.Name = "lblCarne";
+            this.lblCarne.Size = new System.Drawing.Size(50, 17);
+            this.lblCarne.TabIndex = 10;
+            this.lblCarne.Text = "Carne:";
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtras.Location = new System.Drawing.Point(274, 174);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 33);
+            this.btnAtras.TabIndex = 20;
+            this.btnAtras.Text = "Atras";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(12, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(373, 39);
+            this.lblInfo.TabIndex = 21;
+            this.lblInfo.Text = "Por favor llenar todos los campos con los datos validos del estudiante.";
+            // 
+            // frmAgregarEstudiante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 223);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtGrupo);
+            this.Controls.Add(this.lblGrupo);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCarne);
+            this.Controls.Add(this.lblCarne);
+            this.Name = "frmAgregarEstudiante";
+            this.Text = "Agregar Estudiante - Admin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtGrupo;
+        private System.Windows.Forms.Label lblGrupo;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCarne;
+        private System.Windows.Forms.Label lblCarne;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ ProyectoPrograIV/ProyectoPrograIV/frmAgregarEstudiante.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;//using
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoPrograIV
+{
+    public partial class frmAgregarEstudiante : Form
+    {
+        public frmAgregarEstudiante()
+        {
+            InitializeComponent();
+            this.ActiveControl = txtNombre;//pone el foco en el textbox especificado.
+            this.StartPosition = FormStartPosition.CenterScreen;//pone el form en el centro.
+            txtGrupo.MaxLength = 3;//pone un limite de caracteres al textBox
+            //se instancian en modo lectura los tres text Box
+            txtCarne.ReadOnly = true;
+            txtCorreo.ReadOnly = true;
+            txtGrupo.ReadOnly = true;
+        }//fin constructor
+
+        Logica lg = new Logica();
+
+        //Metodo para generar el carne del estudiante
+        private String generarCarne()
+        {
+            //Se guarda el valor del año en un variable temporal.
+            int anno = DateTime.Now.Year;
+
+            //Se crea un numero aleatorio para finalizar el carne
+            Random random = new Random();
+            int num = random.Next(1, 10000);//Se crea un un numero de cuatro digitos
+
+            string grupo = txtGrupo.Text;
+            grupo = grupo.Replace("-", "");
+            //se crea un carne para el estudiante poniendo el año de matricula, el grupo y el random.
+            String carne = anno.ToString() + grupo + num;
+
+            return carne;
+        }//fin metodo String generarCarne()
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            //se valida que txtNombre no este vacio y eso decide si se habilita txtCorreo
+            lg.ValidarTextBoxDobleVacio(txtNombre, txtCorreo);
+        }//fin evento txtNombre_TextChanged
+
+        private void txtCorreo_TextChanged(object sender, EventArgs e)
+        {
+            //se valida que txtCorreo no este vacio y eso decide si se habilita txtGrupo
+            lg.ValidarTextBoxDobleVacio(txtCorreo, txtGrupo);
+        }//fin evento txtCorreo_TextChanged
+
+        private void txtGrupo_TextChanged(object sender, EventArgs e)
+        {
+            //llama al metodo para que genere un carne para el estudiante y lo muestre en el textBox.
+            txtCarne.Text = generarCarne();
+        }//fin evento txtGrupo_TextChanged
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //se limpian los textBox
+            txtNombre.Text = "";
+            txtCorreo.Text = "";
+            txtGrupo.Text = "";
+            txtCarne.Text = "";
+            this.ActiveControl = txtNombre;//deja el cursor en text box especificado
+        }//fin evento btnLimpiar_Click
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //Se valida que txtNombre no este vacio
+            if (!lg.EsTextBoxVacio(txtNombre))
+            {
+                //se valida que txtGrupo sea un valor numerico
+                if (lg.EsTextBoxNumerico(txtGrupo))
+                {
+                    OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + "\\Escuela.accdb");
+                    //Statement de SQL para hcer inserciones a la DB que tenemos en access
+                    string query = "INSERT INTO Estudiantes(Carne,Nombre,Correo,Grupo) values('" + txtCarne.Text + "','" + txtNombre.Text + "','" + txtCorreo.Text + "','" + txtGrupo.Text + "')";
+                    //Incializacion del OleDbCommand con los parametros del query y conexion
+                    OleDbCommand orden = new OleDbCommand(query, conexion);
+
+                    orden.Connection.Open(); //Se abre la base de datos y se prepara para modificacion
+                    orden.ExecuteNonQuery(); //Se ejecuta el query SQL deseado
+                    orden.Connection.Close();//Se cierra la base de datos
+                }//fin if txtGrupo
+            }//fin if  if txtNombre
+            else
+            {
+                MessageBox.Show("Por favor llenar todos los campos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }//fin else txtNombre
+        }//fin evento btnAgregar_Click
+    }
+}
